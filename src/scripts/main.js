1f9132af9e04cfb43be26d0eca2b649f7fab120b
aspasov_ <aspasov_@abv.bg>
2025-12-01 18:04:16 +0200

    calc: include logic modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,13 @@ import {
     initModeSwitch,
     calcKeypadToggle,
 } from './calc-buttons.js';
+import {initInputsListener} from './calculations.js';
+import { initNotifications } from './notifications.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     initThemeSwitch();
     initModeSwitch();
+    initNotifications();
+    initInputsListener();
     calcKeypadToggle();
 });
